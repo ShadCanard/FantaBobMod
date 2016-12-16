55400c9a26f58f2494d4eea6becda7a45965a094
Shadt <guillaume.denisart@icloud.com>
2016-12-16 07:41:28 +0100

    FIXED : Entities working, now.

--- src/main/java/com/shadcanard/fbm/init/ModEntities.java
+++ src/main/java/com/shadcanard/fbm/init/ModEntities.java
@@ -20,27 +20,27 @@ public class ModEntities {
 
 
 
-        EntityRegistry.registerModEntity(new ResourceLocation("fbm"), EntityBob.class, Names.Entities.BOB, 0, FantaBobMod.instance,40,1,true,new Color(180, 0, 0).getRGB(), new Color(255,182,90).getRGB());
+        EntityRegistry.registerModEntity(new ResourceLocation("fbm",Names.Entities.BOB), EntityBob.class, Names.Entities.BOB, 1, FantaBobMod.instance,40,1,true,new Color(180, 0, 0).getRGB(), new Color(255,182,90).getRGB());
         EntityRegistry.addSpawn(EntityBob.class,15,0,1, EnumCreatureType.CREATURE,Biomes.FOREST, Biomes.BIRCH_FOREST,Biomes.BEACH);
 
-        EntityRegistry.registerModEntity(new ResourceLocation("fbm"), EntityFanta.class, Names.Entities.FANTA, 1, FantaBobMod.instance, 40, 1, true,new Color(0,0,230).getRGB(), new Color(0,0,0).getRGB());
+        EntityRegistry.registerModEntity(new ResourceLocation("fbm",Names.Entities.FANTA), EntityFanta.class, Names.Entities.FANTA, 2, FantaBobMod.instance, 40, 1, true,new Color(0,0,230).getRGB(), new Color(0,0,0).getRGB());
         EntityRegistry.addSpawn(EntityFanta.class,15,0,1, EnumCreatureType.CREATURE, Biomes.FOREST, Biomes.BIRCH_FOREST,Biomes.BEACH);
 
-        EntityRegistry.registerModEntity(new ResourceLocation("fbm"), EntityJeanKevin.class, Names.Entities.JEAN_KEVIN, 2, FantaBobMod.instance,40,1,true, new Color(50, 210, 150).getRGB(), new Color(50, 50, 255).getRGB());
+        EntityRegistry.registerModEntity(new ResourceLocation("fbm",Names.Entities.JEAN_KEVIN), EntityJeanKevin.class, Names.Entities.JEAN_KEVIN, 3, FantaBobMod.instance,40,1,true, new Color(50, 210, 150).getRGB(), new Color(50, 50, 255).getRGB());
         EntityRegistry.addSpawn(EntityJeanKevin.class,15,0,1, EnumCreatureType.CREATURE, Biomes.FOREST, Biomes.BIRCH_FOREST,Biomes.BEACH);
 
-        EntityRegistry.registerModEntity(new ResourceLocation("fbm"), EntityBot.class, Names.Entities.BOT, 3, FantaBobMod.instance,40,1,true, new Color(255, 182, 90).getRGB(), new Color(180, 0, 0).getRGB());
+        EntityRegistry.registerModEntity(new ResourceLocation("fbm",Names.Entities.BOT), EntityBot.class, Names.Entities.BOT, 4, FantaBobMod.instance,40,1,true, new Color(255, 182, 90).getRGB(), new Color(180, 0, 0).getRGB());
         EntityRegistry.addSpawn(EntityBot.class, 15,0,1, EnumCreatureType.MONSTER, Biomes.FOREST, Biomes.BIRCH_FOREST,Biomes.BEACH);
 
-        EntityRegistry.registerModEntity(new ResourceLocation("fbm"), EntityFemme.class, Names.Entities.FEMME, 4, FantaBobMod.instance, 40,1,true, new Color(255,150,150).getRGB(), new Color(255,100,100).getRGB());
+        EntityRegistry.registerModEntity(new ResourceLocation("fbm",Names.Entities.FEMME), EntityFemme.class, Names.Entities.FEMME, 5, FantaBobMod.instance, 40,1,true, new Color(255,150,150).getRGB(), new Color(255,100,100).getRGB());
         EntityRegistry.addSpawn(EntityFemme.class,15,0,1, EnumCreatureType.CREATURE, Biomes.FOREST, Biomes.BIRCH_FOREST,Biomes.BEACH);
 
-        EntityRegistry.registerModEntity(new ResourceLocation("fbm"), EntityBossLennon.class, Names.Entities.BOSS_LENNON, 5, FantaBobMod.instance, 40,1,true, new Color(180, 0, 0).getRGB(), new Color(255, 182, 90).getRGB());
+        EntityRegistry.registerModEntity(new ResourceLocation("fbm",Names.Entities.BOSS_LENNON), EntityBossLennon.class, Names.Entities.BOSS_LENNON, 6, FantaBobMod.instance, 40,1,true, new Color(180, 0, 0).getRGB(), new Color(255, 182, 90).getRGB());
 
         //Adding Buddies
-        EntityRegistry.registerModEntity(new ResourceLocation("fbm"), BuddyBob.class, Names.Entities.BUDDY_BOB,6,FantaBobMod.instance,40,1,true);
-        EntityRegistry.registerModEntity(new ResourceLocation("fbm"), BuddyFanta.class,Names.Entities.BUDDY_FANTA,7,FantaBobMod.instance,40,1,true);
-        EntityRegistry.registerModEntity(new ResourceLocation("fbm"), BuddyTipeee.class,Names.Entities.BUDDY_TIPEEE,8,FantaBobMod.instance,40,1,true);
+        EntityRegistry.registerModEntity(new ResourceLocation("fbm",Names.Entities.BUDDY_BOB), BuddyBob.class, Names.Entities.BUDDY_BOB,7,FantaBobMod.instance,40,1,true);
+        EntityRegistry.registerModEntity(new ResourceLocation("fbm",Names.Entities.BUDDY_FANTA), BuddyFanta.class,Names.Entities.BUDDY_FANTA,8,FantaBobMod.instance,40,1,true);
+        EntityRegistry.registerModEntity(new ResourceLocation("fbm",Names.Entities.BUDDY_TIPEEE), BuddyTipeee.class,Names.Entities.BUDDY_TIPEEE,9,FantaBobMod.instance,40,1,true);
 
     }
 }
